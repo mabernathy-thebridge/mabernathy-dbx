ab8bcd96c5017d67466bb13af87a349328038043
mabernathy-thebridge <matthew.abernathy@thebridge.com>
2025-03-11 11:43:19 -0500

    partsupp fix

--- configs/gold/fact_partsupp.sql
+++ configs/gold/fact_partsupp.sql
@@ -1,9 +1,8 @@
-
-
-
-
-
-
-
-
-FROM practice_sandbox.ma_sandbox.partsupp    current_timestamp() as silver_update_timestamp    ps_comment as partsupp_comment,    ps_supplycost as supply_cost,    ps_availqty as available_quantity,    ps_suppkey as supplier_key,    ps_partkey as part_key,SELECT 
+SELECT
+    ps_partkey as part_key,
+    ps_suppkey as supplier_key,
+    ps_availqty as available_quantity,
+    ps_supplycost as supply_cost,
+    ps_comment as partsupp_comment,
+    current_timestamp() as silver_update_timestamp
+FROM practice_sandbox.ma_sandbox.partsupp
